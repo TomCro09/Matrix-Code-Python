c84b93b9b8959f9dd6ab31a75333d29378676236
Tom Cronin <TJCcronin@protonmail.com>
2023-12-12 15:55:48 +0000

    Add files via upload

---
+++ Matrixcode.py
@@ -0,0 +1,57 @@
+import os
+import time
+import random
+
+os.system('color 0a')
+
+nums = [1,0]
+tm = 0
+
+while tm < 30:
+    print(random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),random.randrange(1,5)* " ",
+    random.choice(nums),)
+
+    tm = tm + 0.1
+
+    time.sleep(0.1)
+
+print("Your are not safe")
+print("I see your every movement")
+print("Try and hide, it wont help you")
